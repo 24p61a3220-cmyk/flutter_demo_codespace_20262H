538ac9e5f30d44d7494cc45ed7746ff06991f13d
Flutter Hyderabad <114855672+FlutterHyd@users.noreply.github.com>
2026-08-01 09:32:37 +0000

    added assets

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,16 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
+            Container(
+              height: 48,
+              child: Image.asset("assets/images/flutter_logo.png"),
+            ),
+            const Text('My Flutter DEMO IN GITHUB CODESPACE'),
             const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
